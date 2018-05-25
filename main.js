edef7267fbf8918818e965ab65a85e3002140f16
Aleksey <kedraja@gmail.com>
2018-05-25 14:42:13 +0300

    mstr

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-var arr = [[ "", "", "",]]
-   ;var table = '';
-    var rows = 7;
-    var cols = 11;
-    for (var r = 0; r < rows; r++) {
-        table += '<tr>';
-        for (var c = 1; c <= cols; c++) {
-            table += '<td>' + c + '</td>';
-        }
-        table += '</tr>';
-    }
+var bennett = {
+    _id: "5afea64553cc33b6b254ebb5",
+    index: 1,
+    guid: "19f3c372-6351-4622-9ffe-cfeb7529d704",
+    isActive: false,
+    balance: "$1,491.82",
+    picture: "http://placehold.it/32x32",
+    age: 40,
+    eyeColor: "blue",
+    name: "Bennett Fry",
+    gender: "male",
+    company: "DYNO",
+    email: "bennettfry@dyno.com",
+    phone: "+1 (874) 598-2214",
+}
+var pittman = {
+        _id: "5afea6459812c507b9c68606",
+        index: 0,
+        guid: "bf1f589c-1ea0-4211-8c5f-8ecff01266cd",
+        isActive: false,
+        balance: "$2,398.22",
+        picture: "http://placehold.it/32x32",
+        age: 20,
+        eyeColor: "blue",
+        name: "Pittman Hendricks",
+        gender: "male",
+        company: "ENERSAVE",
+        email: "pittmanhendricks@enersave.com",
+        phone: "+1 (822) 597-3562",
+        address: "686 Brighton Avenue, Takilma, Arkansas, 8323",
+}
 
-    document.write('<table border="1">' + table + '</table>');
+
+   var tb = document.getElementById(table);
+
+    document.createElement(table.address);
+
+
 
 
