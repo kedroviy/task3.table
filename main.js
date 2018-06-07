6fdb399939052f9a3d6b6708c86c5f40045c0949
Aleksey <kedraja@gmail.com>
2018-06-07 14:16:57 +0300

    how to a draw table?

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var jsan = [{
-        "_id": "5afea64553cc33b6b254ebb5",
-        "index": 1,
-        "guid": "19f3c372-6351-4622-9ffe-cfeb7529d704",
-        "isActive": false,
-        "balance": "$1,491.82",
-        "picture": "http://placehold.it/32x32",
-        "age": 40,
-        "eyeColor": "blue",
-        "name": "Bennett Fry",
-        "gender": "male",
-        "company": "DYNO",
-        "email": "bennettfry@dyno.com",
-        "phone": "+1 (874) 598-2214",
-        "address": "Vetka, Fabrichnay, come on drink beer",},
+    "_id": "5afea64553cc33b6b254ebb5",
+    "index": 1,
+    "guid": "19f3c372-6351-4622-9ffe-cfeb7529d704",
+    "isActive": false,
+    "balance": "$1,491.82",
+    "picture": "http://placehold.it/32x32",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": "Bennett Fry",
+    "gender": "male",
+    "company": "DYNO",
+    "email": "bennettfry@dyno.com",
+    "phone": "+1 (874) 598-2214",
+    "address": "Vetka, Fabrichnay, come on drink beer"
+},
     {
         "_id": "5afea6459812c507b9c68606",
         "index": 0,
@@ -27,8 +28,10 @@ var jsan = [{
         "company": "ENERSAVE",
         "email": "pittmanhendricks@enersave.com",
         "phone": "+1 (822) 597-3562",
-        "address": "686 Brighton Avenue, Takilma, Arkansas, 8323",},
-    {"_id": "5afea645e11a634e5f6c6767",
+        "address": "686 Brighton Avenue, Takilma, Arkansas, 8323"
+    },
+    {
+        "_id": "5afea645e11a634e5f6c6767",
         "index": 3,
         "guid": "95d7e5df-9398-4098-96c4-c3b40b469c3d",
         "isActive": true,
@@ -41,9 +44,11 @@ var jsan = [{
         "company": "POOCHIES",
         "email": "mirandadavid@poochies.com",
         "phone": "+1 (896) 461-3051",
-        "address": "468 Clinton Street, Bentonville, New Hampshire, 9125",},
+        "address": "468 Clinton Street, Bentonville, New Hampshire, 9125"
+    },
 
-    {"_id": "5afea64578945e38305ae249",
+    {
+        "_id": "5afea64578945e38305ae249",
         "index": 4,
         "guid": "43450e72-ee74-4c08-95c9-023f7119426a",
         "isActive": true,
@@ -56,56 +61,64 @@ var jsan = [{
         "company": "SPLINX",
         "email": "stacyfrank@splinx.com",
         "phone": "+1 (910) 542-2988",
-        "address": "488 Emmons Avenue, Imperial, American Samoa, 1524",},
-    {"_id": "5afea645fbfcc7026f1dadeb",
-    "index": 5,
-    "guid": "5e1bdb32-a8e8-474e-8bb0-1ba64b247ebe",
-    "isActive": true,
-    "balance": "$3,699.45",
-    "picture": "http://placehold.it/32x32",
-    "age": 36,
-    "eyeColor": "green",
-    "name": "Christi Zamora",
-    "gender": "female",
-    "company": "VELOS",
-    "email": "christizamora@velos.com",
-    "phone": "+1 (935) 486-2015",
-    "address": "576 Hausman Street, Wattsville, Connecticut, 6328",}];
+        "address": "488 Emmons Avenue, Imperial, American Samoa, 1524"
+    },
+    {
+        "_id": "5afea645fbfcc7026f1dadeb",
+        "index": 5,
+        "guid": "5e1bdb32-a8e8-474e-8bb0-1ba64b247ebe",
+        "isActive": true,
+        "balance": "$3,699.45",
+        "picture": "http://placehold.it/32x32",
+        "age": 36,
+        "eyeColor": "green",
+        "name": "Christi Zamora",
+        "gender": "female",
+        "company": "VELOS",
+        "email": "christizamora@velos.com",
+        "phone": "+1 (935) 486-2015",
+        "address": "576 Hausman Street, Wattsville, Connecticut, 6328"
+    }
+    ]
 
-    // jsan.splice(3,"");
-    var cont = jsan.parse;
+// var prs = JSON.parse(jsan);
 var tb = document.getElementById('table');
 var th = document.createElement('tr');
 var btn = document.createElement('button');
+var newSort = document.createElement('tr');
 
-function srt() {
-    jsan.forEach(function (value) {
-            jsan.sort();
-            console.log(value.name)
-            document.getElementById("table").innerHTML += '<td>' + value.balance + '</td>';
+function srt(jsan) {
+            jsan.sort (function (a, b) {
+                if (a.balance > b.balance) return 1
 
-        // row.innerHTML += '<td>' + value._id + '</td>';
-        // row.innerHTML += '<td>' + value.index + '</td>';
-        // row.innerHTML += '<td>' + value.guid + '</td>';
-        // row.innerHTML += '<td>' + value.isActive + '</td>';
-        // row.innerHTML += '<td>' + value.balance + '</td>';
-        // row.innerHTML += '<td>' + value.picture + '</td>';
-        // row.innerHTML += '<td>' + value.age + '</td>';
-        // row.innerHTML += '<td>' + value.eyeColor + '</td>';
-        // row.innerHTML += '<td>' + value.name + '</td>';
-        // row.innerHTML += '<td>' + value.gender + '</td>';
-        // row.innerHTML += '<td>' + value.company + '</td>';
-        // row.innerHTML += '<td>' + value.email + '</td>';
-        // row.innerHTML += '<td>' + value.phone + '</td>';
-        // row.innerHTML += '<td>' + value.address + '</td>';
-        //
-        // tb.appendChild(row);
-        console.log(1);
+
+            tb.appendChild(th).appendChild(btn);
+                jsan.forEach(function (value) {
+                    var sortRaw = document.createElement('tr');
+                    sortRaw.innerHTML += '<td>' + value._id + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.index + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.guid + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.isActive + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.balance + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.picture + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.age + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.eyeColor + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.name + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.gender + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.company + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.email + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.phone + '</td>';
+                    sortRaw.innerHTML += '<td>' + value.address + '</td>';
+                    tb.appendChild(sortRaw);
+                    tb.removeChild(tb);
+
+                })
+        console.log(jsan);
         });
         }
 Object.keys(jsan[0]).forEach(function (value) {
     // var obj = Object.keys(value);
-    th.innerHTML += '<th>' + value + '<input type="button" onclick="srt()" class="sort ' + value + ' "> </input>' + '</th>';
+    th.innerHTML += '<th>' + value + '<input type="button" onclick="srt(jsan)" id="sortd" ' + value + ' "> </input>' + '</th>';
 
     // th.innerHTML += '<th>' +  + '<input type="button" class="sortIndex"> </input>' + '</th>';
     // th.innerHTML += '<th>' + obj[2] + '<input type="button" class="sortGiud"> </input>' + '</th>';
@@ -147,27 +160,3 @@ tb.appendChild(th).appendChild(btn);
 
 
     });
-// jsan.forEach(function (value) {
-//     var sortRow = document.createElement('tr');
-//     sortRow.innerHTML += '<td>' + value._id + jsan.sort() + '</td>';
-//     document.body.insertBefore(sortRow, tr);
-//     console.log(1);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-   //
-   //  document.createElement(table.address);
-
-
-
-
